a26ab23047afcf9afe78fefab4b217583f471e74
nmquan22 <128149552+nmquan22@users.noreply.github.com>
2024-11-09 12:26:44 +0700

    change getSuccestor and add write output

--- Ares/game_ui.py
+++ Ares/game_ui.py
@@ -4,6 +4,8 @@ import itertools
 from game import MazeGame
 import time
 from utils import parse_input
+import re
+import tracemalloc
 class MazeGameUI:
     def __init__(self, root,grid, stone_weights):
         self.root = root
@@ -16,6 +18,7 @@ class MazeGameUI:
         self.label_cost.pack()
         self.goal_reached=False
         self.cell_size = 50
+        self.level = 2 
         self.animation_speed = 100  # Default speed for idle animation
 
         self.images = {
@@ -94,6 +97,9 @@ class MazeGameUI:
             self.goal_reached = False  # Reset goal state          
             self.game = None  # Remove the current game instance
             gird, stone_weights = parse_input(filename)
+            match = re.search(r'\d+',filename)
+            if match:
+                self.level = match.group()
             self.game = MazeGame(gird, stone_weights)
             self.reset_animation()
             self.draw_grid()
@@ -224,9 +230,16 @@ class MazeGameUI:
             print("No solution")
 
     def ucs(self):
-        solution_path=self.game.bfs()
+        result=self.game.ucs()
+        solution_path = result["solution_path"]
         if solution_path:
             self.simulate_solution(solution_path)
+            steps = result["steps"]
+            nodes_generated = result["nodes_generated"]
+            cost = result["cost"]
+            time_ms = result["time_ms"]
+            memory_mb = result["memory_mb"]
+            self.write_output("UCS", solution_path, steps, cost, nodes_generated,time_ms,memory_mb)
         else:
             print("No solution")
 
@@ -242,7 +255,10 @@ class MazeGameUI:
         def step(index):
             if index < len(solution_path):
             # Mapping the direction and making the move
-                direction_map = {'U': (-1, 0), 'D': (1, 0), 'L': (0, -1), 'R': (0, 1)}
+                direction_map = {
+                'U': (-1, 0), 'D': (1, 0), 'L': (0, -1), 'R': (0, 1),
+                'u': (-1, 0), 'd': (1, 0), 'l': (0, -1), 'r': (0, 1)
+                }
                 move_dir = solution_path[index]
                 dr, dc = direction_map[move_dir]
                 self.move((dr, dc))  # Move within the UI
@@ -252,4 +268,18 @@ class MazeGameUI:
 
         # Start the first step
         step(0)
+        
+    def write_output(self,algorithm_name,solution_path, steps, cost, nodes_generated,time_ms,memory_mb):
+    
+        # Prepare the output file path
+        output_filename = f"output-{self.level}.txt"
 
+        # Prepare solution details for output
+        solution_string = ''.join(solution_path)  
+
+        # Write to output file
+        with open(output_filename, 'w') as f:
+            f.write(f"{algorithm_name}\n")
+            f.write(f"Steps: {steps}, Cost: {cost}, Node: {nodes_generated}, "
+                    f"Time (ms): {time_ms:.2f}, Memory (MB): {memory_mb:.2f}\n")
+            f.write(solution_string)
